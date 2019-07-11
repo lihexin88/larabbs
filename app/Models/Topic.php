8b6fa19b7b9f323a9aa9241ed02b097d92d11595
李贺鑫 <228795709@qq.com>
2019-07-11 14:31:18 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,7 +2,27 @@
 
 namespace App\Models;
 
-class Topic extends Model
-{
+class Topic extends Model {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * 以下两个方法，为定义topic模型的对应关系，为数据库中的外键
+     */
+
+    /**
+     * 一个话题属于一个分类
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+
+    /**
+     * 一个话题属于一个用户
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
