999bbcce01464367efa6567cbd896cb17d586009
李贺鑫 <228795709@qq.com>
2019-07-11 17:20:27 +0800

    用户可以编辑话题

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -5,16 +5,12 @@ namespace App\Policies;
 use App\Models\User;
 use App\Models\Topic;
 
-class TopicPolicy extends Policy
-{
-    public function update(User $user, Topic $topic)
-    {
-        // return $topic->user_id == $user->id;
-        return true;
+class TopicPolicy extends Policy {
+    public function update(User $user, Topic $topic) {
+        return $topic->user_id == $user->id;
     }
 
-    public function destroy(User $user, Topic $topic)
-    {
+    public function destroy(User $user, Topic $topic) {
         return true;
     }
 }
