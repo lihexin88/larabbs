ef912af076bf9ecc52c6d4746db1619cfa60d52e
李贺鑫 <228795709@qq.com>
2019-07-11 15:07:05 +0800

    话题排序

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -12,8 +12,8 @@ class TopicsController extends Controller {
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index() {
-        $topics = Topic::with('user', 'category')->paginate(30);
+    public function index(Request $request, Topic $topic) {
+        $topics = $topic->withOrder($request->order)->paginate(20);
         return view('topics.index', compact('topics'));
     }
 
