c8c21efcc54a7e4a47aa59b97db0b5ba6d2ad569
李贺鑫 <228795709@qq.com>
2019-07-12 19:01:58 +0800

    用户管理后台

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,4 +73,34 @@ class User extends Authenticatable implements MustVerifyEmailContract {
         $this->save();
         $this->unreadNotifications->markAsRead();
     }
+
+    /**
+     * 设置用户密码时加密
+     * @param $value
+     */
+    public function setPasswordAttribute($value) {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    /**
+     * 管理端上传用户头像，修改用户头像问题
+     * @param $path
+     */
+    public function setAvatarAttribute($path) {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if (!starts_with($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
 }
