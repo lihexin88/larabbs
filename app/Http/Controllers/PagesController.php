a947bef6f0544d6e759724ced7f8253f4eb74724
李贺鑫 <228795709@qq.com>
2019-07-12 19:26:09 +0800

    管理后台权限

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,4 +8,17 @@ class PagesController extends Controller {
     public function root() {
         return view('pages.root');
     }
+
+    /**
+     * 用户登录后台管理端被拒绝后，执行如下操作
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View
+     */
+    public function permissionDenied() {
+        // 如果当前用户有权限访问后台，直接跳转访问
+        if (config('administrator.permission')()) {
+            return redirect(url(config('administrator.uri')), 302);
+        }
+        // 否则使用视图
+        return view('pages.permission_denied');
+    }
 }
