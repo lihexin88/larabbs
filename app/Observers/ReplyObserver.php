e2f62311dd8237ebfb546ec49dac7cc99c12841f
李贺鑫 <228795709@qq.com>
2019-07-12 10:12:47 +0800

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -7,15 +7,22 @@ use App\Models\Reply;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
-class ReplyObserver
-{
-    public function creating(Reply $reply)
-    {
-        //
+class ReplyObserver {
+    /**
+     * 事件监控，在创建回复之后，进行操作
+     * @param Reply $reply
+     */
+    public function created(Reply $reply) {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 
-    public function updating(Reply $reply)
-    {
-        //
+    /**
+     *事件监控，在新的回复创建的时候，进行操作
+     * 对回复的提交数据过滤
+     * @param Reply $reply
+     */
+    public function creating(Reply $reply) {
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
