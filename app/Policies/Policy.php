600c05d0256f4948e1bfdeefcacc472cc68f8bc8
李贺鑫 <228795709@qq.com>
2019-07-12 17:20:12 +0800

    站点权限部署

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -4,19 +4,13 @@ namespace App\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class Policy
-{
+class Policy {
     use HandlesAuthorization;
 
-    public function __construct()
-    {
-        //
+    public function before($user, $ability) {
+        // 如果用户拥有管理内容的权限的话，即授权通过
+        if ($user->can('manage_contents')) {
+            return true;
+        }
     }
-
-    public function before($user, $ability)
-	{
-	    // if ($user->isSuperAdmin()) {
-	    // 		return true;
-	    // }
-	}
 }
