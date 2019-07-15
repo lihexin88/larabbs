8d864fe12a352067d48998bfd2214cd1b47df88c
李贺鑫 <228795709@qq.com>
2019-07-15 17:32:24 +0800

    api/user

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
 
-class UsersController extends Controller
-{
-    public function store(UserRequest $request)
-    {
+
+class UsersController extends Controller {
+
+    /**
+     * 用户注册 控制器
+     * @param UserRequest $request
+     * @return \Dingo\Api\Http\Response|void
+     */
+    public function store(UserRequest $request) {
         $verifyData = \Cache::get($request->verification_key);
 
         if (!$verifyData) {
@@ -30,6 +36,16 @@ class UsersController extends Controller
         // 清除验证码缓存
         \Cache::forget($request->verification_key);
 
-        return $this->response->created();
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
+    }
+
+    public function me() {
+        return $this->response->item($this->user(), new UserTransformer());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ $api->version('v1', [
 
 //用户登录
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function ($api) {
     // 短信验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')
@@ -47,15 +48,17 @@ $api->version('v1', [
 
     $api->group([
         'middleware' => 'api.throttle',
-        'limit' => config('api.rate_limits.sign.limit'),
-        'expires' => config('api.rate_limits.sign.expires'),
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
     ], function ($api) {
-        // 短信验证码
-        $api->post('verificationCodes', 'VerificationCodesController@store')
-            ->name('api.verificationCodes.store');
-        // 用户注册
-        $api->post('users', 'UsersController@store')
-            ->name('api.users.store');
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function ($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
     });
 });
 
