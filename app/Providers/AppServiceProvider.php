d4d9b053bfbda9688460ac328b93e47eb6ebaf72
李贺鑫 <228795709@qq.com>
2019-07-15 09:02:47 +0800

    侧边栏链接

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,15 +4,13 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         if (app()->isLocal()) {
             $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
         }
@@ -23,12 +21,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-	{
-		\App\Models\User::observe(\App\Observers\UserObserver::class);
-		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
-		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
-
+    public function boot() {
+        \App\Models\User::observe(\App\Observers\UserObserver::class);
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+        \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
         //
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,12 +3,10 @@
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
-    /**
-     *按照顺序进行批量数据填充
-     */
     public function run() {
         $this->call(UsersTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
-		$this->call(ReplysTableSeeder::class);
+        $this->call(ReplysTableSeeder::class);
+        $this->call(LinksTableSeeder::class);
     }
 }
