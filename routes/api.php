d8495239c2d7ca447915226a9726e1a8cca8652d
李贺鑫 <228795709@qq.com>
2019-07-15 14:02:03 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
-], function($api) {
+], function ($api) {
     // 短信验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')
         ->name('api.verificationCodes.store');
@@ -15,7 +15,7 @@ $api->version('v1', [
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
-], function($api) {
+], function ($api) {
     // 短信验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')
         ->name('api.verificationCodes.store');
@@ -23,3 +23,25 @@ $api->version('v1', [
     $api->post('users', 'UsersController@store')
         ->name('api.users.store');
 });
+
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api',
+], function ($api) {
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function ($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
+});
+
+
+
