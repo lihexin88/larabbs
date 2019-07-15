81091ed848d9b05d5172f3bc65be80c34cea8391
李贺鑫 <228795709@qq.com>
2019-07-15 14:48:47 +0800

    add socialiteproviders

--- config/services.php
+++ config/services.php
@@ -50,4 +50,9 @@ return [
     ],
 
 
+    'weixin' => [
+        'client_id' => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect' => env('WEIXIN_REDIRECT_URI'),
+    ],
 ];
