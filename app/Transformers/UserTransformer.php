975c7ec1c21512af3acc84050417d7afa7d2bf26
李贺鑫 <228795709@qq.com>
2019-07-16 13:55:06 +0800

    include user roles

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -5,19 +5,29 @@ namespace App\Transformers;
 use App\Models\User;
 use League\Fractal\TransformerAbstract;
 
-class UserTransformer extends TransformerAbstract {
-    public function transform(User $user) {
+class UserTransformer extends TransformerAbstract
+{
+    protected $availableIncludes = ['roles'];
+
+    public function transform(User $user)
+    {
         return [
-            'id' => $user->id,
-            'name' => $user->name,
-            'email' => $user->email,
-            'avatar' => $user->avatar,
-            'introduction' => $user->introduction,
-            'bound_phone' => $user->phone ? true : false,
-            'bound_wechat' => ($user->weixin_unionid || $user->weixin_openid) ? true : false,
+            'id'              => $user->id,
+            'name'            => $user->name,
+            'email'           => $user->email,
+            'avatar'          => $user->avatar,
+            'introduction'    => $user->introduction,
+            'bound_phone'     => $user->phone ? true : false,
+            'bound_wechat'    => ($user->weixin_unionid || $user->weixin_openid) ? true : false,
             'last_actived_at' => $user->last_actived_at->toDateTimeString(),
-            'created_at' => (string)$user->created_at,
-            'updated_at' => (string)$user->updated_at,
+            'created_at'      => (string)$user->created_at,
+            'updated_at'      => (string)$user->updated_at,
         ];
+    }
+
+
+    public function includeRoles(User $user)
+    {
+        return $this->collection($user->roles, new RoleTransformer());
     }
 }
