23e18e4c5a52578124635378b89ad9d36e3179de
李贺鑫 <228795709@qq.com>
2019-07-16 11:09:12 +0800

    replies destroy

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -9,6 +9,13 @@ use App\Http\Requests\Api\ReplyRequest;
 
 class RepliesController extends Controller
 {
+    /**
+     * 发布回复
+     * @param ReplyRequest $request
+     * @param Topic $topic
+     * @param Reply $reply
+     * @return \Dingo\Api\Http\Response
+     */
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {
         $reply->content = $request->content;
@@ -17,5 +24,24 @@ class RepliesController extends Controller
         $reply->save();
 
         return $this->response->item($reply, new ReplyTransformer())->setStatusCode(201);
+    }
+
+    /**
+     * 删除回复
+     * @param Topic $topic
+     * @param Reply $reply
+     * @return \Dingo\Api\Http\Response|void
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            return $this->response->errorBadRequest();
+        }
+
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return $this->response->noContent();
     }
 }
